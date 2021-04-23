941454f43570a4451446d2831b4448627abd4812
Jayage <javin.li@gmail.com>
2021-04-23 11:33:53 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function getData(cb){
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://swapi.dev/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState==4 &&this.status == 200){
+            cb(JSON.parse(this.responseText));
+
+        }
+    };
+}
+
+function writeToDocument(type){
+    getData(type, function(data){
+        document.getElementById("data").innerHTML = data;
+    })
+}
